718d7345ccad70909396a75522258e6188f23434
timurlauttt <iripyoga@gmail.com>
2026-01-19 21:56:34 +0700

    Translate entire blog to Indonesian
    - Beranda (Home)
    - Postingan Terbaru (Latest Posts)
    - Postingan Terkait (Related Posts)
    - Bagikan (Share)
    - Dipublikasikan (Published)
    - Tags
    - Search component messages
    - All UI text now in Indonesian

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -13,9 +13,9 @@ export const siteConfig: SiteConfig = {
 	site: 'https://your-blog.vercel.app/', // Write here your website url (update after deployment)
 	author: 'Urip Yoga Pangestu', // Site author
 	title: 'Blog - Urip Yoga Pangestu', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
+	description: 'Blog pribadi Urip Yoga Pangestu tentang kehidupan sehari-hari dan teknologi.', // Description to display in the meta tags
+	lang: 'id-GB',
+	ogLocale: 'id_GB',
+	shareMessage: 'Bagikan postingan ini', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
