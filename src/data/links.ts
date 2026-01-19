16f763241b1bd05c4edc5a7338d42842679bc04b
timurlauttt <iripyoga@gmail.com>
2026-01-19 12:55:59 +0700

    Setup for Vercel deployment

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,21 @@
-import TwitterIcon from '@/components/icons/TwitterIcon'
+import LinkedinIcon from '@/components/icons/LinkedinIcon'
 import GithubIcon from '@/components/icons/GithubIcon'
-
+import InstagramIcon from '@/components/icons/InstagramIcon'
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
 		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://github.com/timurlauttt',
 		icon: GithubIcon
 	},
-
 	{
-		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: TwitterIcon
-	}
+		name: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/urip-yoga-pangestu-65a541231/',
+		icon: LinkedinIcon
+	},
+	{
+		name: 'Instagram',
+		url: 'https://www.instagram.com/__timurlaut',
+		icon: InstagramIcon
+	},
 ] as const
